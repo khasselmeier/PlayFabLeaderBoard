5fbd6b6b0924fdee428b4488ffe0c553d5d9a977
khasselmeier <123027444+khasselmeier@users.noreply.github.com>
2023-11-17 13:04:24 -0700

    fixed leaderboard

--- Leaderboard/Assets/Scripts/Leaderboard.cs
+++ Leaderboard/Assets/Scripts/Leaderboard.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
 using TMPro;
+
 public class Leaderboard : MonoBehaviour
 {
     public GameObject leaderboardCanvas;
     public GameObject[] leaderboardEntries;
-
     public static Leaderboard instance;
     void Awake() { instance = this; }
 
     public void OnLoggedIn()
     {
         leaderboardCanvas.SetActive(true);
+        //SetLeaderboardEntry(-8700);
         DisplayLeaderboard();
     }
 
@@ -25,9 +26,10 @@ public class Leaderboard : MonoBehaviour
             StatisticName = "FastestTime",
             MaxResultsCount = 10
         };
+
         PlayFabClientAPI.GetLeaderboard(getLeaderboardRequest,
-        result => UpdateLeaderboardUI(result.Leaderboard),
-        error => Debug.Log(error.ErrorMessage)
+            result => UpdateLeaderboardUI(result.Leaderboard),
+            error => Debug.Log(error.ErrorMessage)
         );
     }
 
@@ -36,7 +38,9 @@ public class Leaderboard : MonoBehaviour
         for (int x = 0; x < leaderboardEntries.Length; x++)
         {
             leaderboardEntries[x].SetActive(x < leaderboard.Count);
-            if (x >= leaderboard.Count) continue;
+
+            if (x >= leaderboard.Count) 
+                continue;
 
             leaderboardEntries[x].transform.Find("PlayerName").GetComponent<TextMeshProUGUI>().text = (leaderboard[x].Position + 1) + ". " + leaderboard[x].DisplayName;
             leaderboardEntries[x].transform.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = (-(float)leaderboard[x].StatValue * 0.001f).ToString("F2");
@@ -45,6 +49,11 @@ public class Leaderboard : MonoBehaviour
 
     public void SetLeaderboardEntry(int newScore)
     {
+        // NOTE: the original version of this game used server-side automation in Javascript to update
+        // the leaderboard. Microsoft removed that feature for new PlayFab applications, so we'll demonstrate
+        // updating a leaderboard using two different methods below. The 'legacy method' calls the custom server-side
+        // javascript (cloud script). The alternative uses the latest PlayFabClientAPI to update the player's best score
+
         bool useLegacyMethod = false;
 
         if (useLegacyMethod)
@@ -54,23 +63,35 @@ public class Leaderboard : MonoBehaviour
                 FunctionName = "UpdateHighscore",
                 FunctionParameter = new { score = newScore }
             };
-
             PlayFabClientAPI.ExecuteCloudScript(request,
-                result =>
-                {
-                    Debug.Log(result);
-                    DisplayLeaderboard();
-                    Debug.Log(result.ToJson());
-                },
-                error =>
-                {
-                    Debug.Log(error.ErrorMessage);
-                    Debug.Log("ERROR");
-                }
+            result =>
+            {
+                Debug.Log(result);
+                //Debug.Log("SUCCESS");
+                //Debug.Log(result.FunctionName);
+                //Debug.Log(result.FunctionResult);
+                //Debug.Log(result.FunctionResultTooLarge);
+                //Debug.Log(result.Error);
+                DisplayLeaderboard();
+                Debug.Log(result.ToJson());
+            },
+            error =>
+            {
+                Debug.Log(error.ErrorMessage);
+                Debug.Log("ERROR");
+            }
             );
         }
         else
         {
+        // ...and here's how we do it
+        // NOTE: by default, clients can't update player statistics
+        // So for the code below to succeed:
+        // 1. Log into PlayFab (from your web browser)
+        // 2. Select your Title.
+        // 3. Select Settings from the left-menu.
+        // 4. Select the API Features tab.
+        // 5. Find and activate Allow client to post player statistics.
             PlayFabClientAPI.UpdatePlayerStatistics(new UpdatePlayerStatisticsRequest
             {
                 // request.Statistics is a list, so multiple StatisticUpdate objects can be defined if required.
@@ -81,7 +102,7 @@ public class Leaderboard : MonoBehaviour
             },
             result => { Debug.Log("User statistics updated"); },
             error => { Debug.LogError(error.GenerateErrorReport()); }
-           );
+            );
         }
     }
 }
